eeea9a30f367fe0637688a10281eaa7661649db8
Savva Dzhumaliev <pro100prosavva@gmail.com>
2020-04-15 15:29:28 +0400

    TaskInfo component

--- src/Components/TaskList/TasksInfo.jsx
+++ src/Components/TaskList/TasksInfo.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-export function TaskInfo() {
-    return <>
-    </>;
+export function TaskInfo({ tasks }) {
+
+    return <div className="taskInfo">
+        <p className="taskInfo__counter taskInfo__counter_allTasks">
+            { tasks.length }
+        </p>
+
+        <p className="taskInfo__counter taskInfo__counter_completedTasks">
+            { tasks.reduce((completedTasksAmount, task) => {
+                return completedTasksAmount + (task.isCompleted ? 1 : 0);
+            }, 0)}
+        </p>
+    </div>;
 }
