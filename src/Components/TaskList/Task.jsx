a7ad0b3314e9ccfb2ed5444db9d2784ffc66f351
Savva Dzhumaliev <pro100prosavva@gmail.com>
2020-04-15 15:51:00 +0400

    Task component

--- src/Components/TaskList/Task.jsx
+++ src/Components/TaskList/Task.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export function Task({ markAsCompleted, remove }) {
-
+export function Task({ task, remove }) {
+    return <div className="tasklist__task">
+        <p className="tasklist__taskName">{task.name}</p>
+        <p className="tasklist__taskDescription">{task.description}</p>
+        <button
+            className="tasklist__taskRemoveButton"
+            OnClick={() => remove(task)}>
+            &#10006
+        </button>
+    </div>
 }
